a53dd21a4838fed98704ce3129d801845f844913
The-Mushroom-Dev <85519592+The-Mushroom-Dev@users.noreply.github.com>
2022-05-20 20:30:16 +0300

    Add files via upload

--- C++/src/Pause.h
+++ C++/src/Pause.h
@@ -16,4 +16,5 @@ void pause_menu()
     DrawRectangle(0, 0, 1280, 720, BLUE);
     DrawText("Press SPACE to continue", x, y, 30, WHITE);
     DrawTextureEx(logo, position, 0.0f, 20.0f, WHITE);
+    DrawText("Paused", 500, 100, 50, WHITE);
 }
